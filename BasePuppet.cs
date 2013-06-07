0357340e837cf73f032807166b277efcd4e28a35
athenak <athena3750@hotmail.com>
2013-06-06 23:15:09 -0300

    Create BasePuppet.cs
    Mat's code

---
+++ BasePuppet.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class BasePuppet : BaseCharacter {
+  
+	private int mana;
+	private int maxMana;
+	
+	void Start()
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+	
+	public void Activate()
+	{
+
+	}
+	
+	public void Deactivate()
+	{
+		
+	}
+}
