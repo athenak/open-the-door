6fe41b5bf0178505daf9a6dc87ceb50a436d168e
athenak <athena3750@hotmail.com>
2013-06-06 23:16:28 -0300

    Create Player.cs
    Mat's code

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+  
+	private string currentPuppet;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update ()
+	{
+	
+	}
+}
