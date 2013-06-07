70faa6dfd2b7625c3da0648e3927753b43db312d
athenak <athena3750@hotmail.com>
2013-06-06 23:12:43 -0300

    Create BaseCharacter.cs
    Mat's version

---
+++ BaseCharacter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+using AssemblyCSharp;
+
+public class BaseCharacter : MonoBehaviour {
+  
+	private int health;
+	private int maxHealth;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+
+	}
+}
